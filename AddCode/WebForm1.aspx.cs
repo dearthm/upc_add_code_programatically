c81fede0f804c7fb6857eb585f022c76af118edc
dearthm <dearthml@gmail.com>
2017-01-24 13:13:01 -0500

    Cleaned up code in WebForm1

--- AddCode/WebForm1.aspx.cs
+++ AddCode/WebForm1.aspx.cs
@@ -12,59 +12,12 @@ namespace AddCode
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            //HtmlGenericControl my_js = new HtmlGenericControl("script");
-            //my_js.Attributes["async"] = "async";
-            //my_js.Attributes["type"] = "text/javascript";
-            //my_js.Attributes["src"] = "http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
-            //Page.Header.Controls.Add(my_js);
-
-            //HtmlGenericControl my_div = new HtmlGenericControl("div");
-            //my_div.Attributes["id"] = "mike";
-            //Page.Header.Controls.Add(my_div);
-            // my_div.InnerText = "Mike";//Over written by lower statement, this is straight text, below enters actual HTML
-            //my_div.Attributes.Add("class", "container");
-            ////Add content
-            //my_div.InnerHtml = "<h1>Hello <b>World</b></h1>";
-
-            //*************************************************************************
-            //Add divs and controls programatically
-            //Find the body element for later use
-            //The element to be searched must have the runat="server" attribute included
-            Control ctrToFind = this.FindControl("theTitle"); 
-            if (ctrToFind != null)
-            {
-                //Get ctr parent
-                Control ctr2 = ctrToFind.Parent;
-                Response.Write("Parent of the 'thebody' is : " + ctr2.ID);
-            }
-            else
-            {
-                Response.Write("Parent not found");
-            }
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            //Used to find a control and it's parent
-            //Control ctr = FindControl("Button1");
-            //if (ctr!= null)
-            //{
-            //    //Get ctr parent
-            //    Control ctr2 = ctr.Parent;
-            //    Response.Write("Parent of the 'Button1' is : " + ctr2.ID);
-            //}
-            //else
-            //{
-            //    Response.Write("Parent not found");
-            //}
-
             Control ctrToFind = this.FindControl("theBody"); //Find the body element for later use
             if (ctrToFind != null)
             {
                 //Get ctr parent
                 Control ctr2 = ctrToFind.Parent;
-                Response.Write("Parent of the 'Button1' is : " + ctr2.ID);
+                Response.Write("Parent of the 'Body' is : " + ctr2.ID);
             }
             else
             {
@@ -73,7 +26,12 @@ namespace AddCode
 
             //HtmlControl ctrDiv = new HtmlGenericControl("div");
             //ctrDiv.Attributes.Add("id", "myDiv");
-            //ctrToFind.Controls.Add(ctrDiv);
+            //ctrToFind.Controls.Add(ctrDiv); 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
 
 
         }
